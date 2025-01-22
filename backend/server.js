b59e07a090a63ab74546fc5d9cb0c07fc632929b
tasin-2019331009 <tahsinul09@student.sust.edu>
2025-01-22 23:47:46 +0600

    1 - update routes files and paths

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
-import authRoutes from "./Routes/auth.routes.js";
-import userRoutes from "./Routes/user.routes.js";
-import postRoutes from "./Routes/post.routes.js";
+import authRoutes from "./Routes/auth.route.js";
+import userRoutes from "./Routes/user.route.js";
+import postRoutes from "./Routes/post.route.js";
 
 import { connectMongoDB } from "./db/connectMongoDB.js";
 
